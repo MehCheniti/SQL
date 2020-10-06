09d17b89346ebab956cc78c36e4354ffd5ef66c2
MehCheniti <mehdiwx91@hotmail.com>
2020-10-06 02:27:24 +0200

    Did some more progress on the SQL weekly assignment for week 5.

--- IN2090/Ukesoppgave5.sql
+++ IN2090/Ukesoppgave5.sql
@@ -1,3 +1,5 @@
+/* Oppgave 1 */
+
 SELECT *
 FROM Genre;
 
@@ -35,3 +37,37 @@ WHERE filmitem.filmtype LIKE '%C%' AND film.title = 'Love';
 SELECT title
 FROM film NATURAL JOIN filmlanguage
 WHERE filmlanguage.language LIKE '%Norwegian%';
+
+/* Oppgave 2 */
+
+SELECT filmid, filmtype
+FROM filmitem
+WHERE filmid IN (
+  SELECT filmid
+  FROM film
+  WHERE prodyear = '1894');
+
+SELECT firstname
+FROM person
+WHERE person.gender LIKE '%F%' AND person.personid IN (
+  SELECT personid
+  FROM filmparticipation
+  WHERE filmid = '357076' AND parttype LIKE '%cast%');
+
+/* Oppgave 3 */
+
+SELECT genre
+FROM filmgenre NATURAL JOIN film
+WHERE film.title LIKE '%Pirates of the Caribbean: The Legend of Jack Sparrow%';
+
+SELECT genre
+FROM filmgenre NATURAL JOIN film
+WHERE film.filmid = 985057;
+
+SELECT f.title, f.prodyear, fi.filmtype
+FROM filmitem AS fi NATURAL JOIN film AS f
+WHERE prodyear = 1894;
+
+SELECT p.firstname, f.filmid
+FROM person AS p NATURAL JOIN filmparticipation AS f
+WHERE f.filmid = 357076 AND p.gender LIKE '%F%' AND f.parttype LIKE '%cast%';
